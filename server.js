926afc982dc3d857f0b35f7317b11c2948f084fe
NickNocc <haretakis4@gmail.com>
2022-01-04 14:57:36 -0800

    Added delete query

--- server.js
+++ server.js
@@ -17,8 +17,22 @@ const db = mysql.createConnection(
     console.log(`Connected to the election database.`)
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, results) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(results);
 })
 
 app.use((req, res) => {
