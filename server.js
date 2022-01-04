bfd5051627d298e7c8f4f111f63c88090eab1f62
NickNocc <haretakis4@gmail.com>
2022-01-04 14:53:16 -0800

    query'd the db from server.js

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: `localhost`,
+        user: `root`,
+        password: `01N3psEaGzdExce3BV97kuWg`,
+        database: `election`
+    },
+    console.log(`Connected to the election database.`)
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 app.use((req, res) => {
     res.status(404).end();
