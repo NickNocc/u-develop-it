870cfcbe8c8bdedab9d663f18a0d6b999f460409
NickNocc <haretakis4@gmail.com>
2022-01-05 09:38:09 -0800

    Added party names to get routes

--- server.js
+++ server.js
@@ -18,9 +18,13 @@ const db = mysql.createConnection(
 );
 
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+                    AS party_name 
+                    FROM candidates 
+                    LEFT JOIN parties 
+                    ON candidates.party_id = parties.id`;
 
-    db.query(`SELECT * FROM candidates`, (err, rows) => {
+    db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
@@ -33,7 +37,13 @@ app.get('/api/candidates', (req, res) => {
 });
 
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                    AS party_name 
+                    FROM candidates 
+                    LEFT JOIN parties 
+                    ON candidates.party_id = parties.id 
+                    WHERE candidates.id = ?`;
+                    
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
